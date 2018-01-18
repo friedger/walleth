1cfd249ee00814281ddf71691f1257f9a77d69b9
Friedger <friedger@gmail.com>
2018-01-18 15:47:37 +0100

    show send container if amount is positive

--- app/src/main/java/org/walleth/activities/MainActivity.kt
+++ app/src/main/java/org/walleth/activities/MainActivity.kt
@@ -140,7 +140,6 @@ class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceCh
         empty_view_container.setVisibility(!hasTransactions && !onboardingController.isShowing)
         transaction_recycler_out.setVisibility(hasTransactions)
         transaction_recycler_in.setVisibility(hasTransactions)
-        send_container.setVisibility(hasTransactions, INVISIBLE)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -249,6 +248,7 @@ class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceCh
 
         if (it != null) {
             value_view.setValue(it.balance, currentTokenProvider.currentToken)
+            send_container.setVisibility(it.balance > ZERO, INVISIBLE)
             supportActionBar?.subtitle = getString(R.string.main_activity_block, it.block)
         } else {
             value_view.setValue(ZERO, currentTokenProvider.currentToken)
